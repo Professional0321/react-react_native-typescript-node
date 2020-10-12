09b2c4ba9ddecfd2045541fd04cd6658badeb07f
Professional0321 <web.dev.mar1212@gmail.com>
2020-10-12 17:34:39 -0300

    adding the map

--- frontend-web/src/pages/OrphanagesMap.tsx
+++ frontend-web/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
 
+import 'leaflet/dist/leaflet.css';
 import '../styles/pages/orphanages-map.css';
 
 import mapMarker from '../img/marker.svg';
@@ -26,7 +28,16 @@ function OrphanagesMap() {
                 </footer>
             </aside>
 
-            <div></div>
+            <Map 
+                center={[-15.8138368,-47.9110414]}
+                zoom={14}
+                style={{
+                    width: '100%',
+                    height: '100%'
+                }}
+            >
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            </ Map>
 
             <Link to="" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
