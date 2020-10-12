8458b787628a4f6912f4483d95f00bcabd8a0724
Professional0321 <web.dev.mar1212@gmail.com>
2020-10-12 17:13:49 -0300

    orphanage map page content

--- frontend-web/src/pages/OrphanagesMap.tsx
+++ frontend-web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import '../styles/pages/orphanages-map.css';
+
+import mapMarker from '../img/marker.svg';
 
 function OrphanagesMap() {
     return (
-        <h1></h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img
+                        src={mapMarker}
+                        alt="Marker"
+                    />
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando pela usa visita :)</p>
+                </header>
+
+                <footer>
+                    <strong>Brasília</strong>
+                    <span>Distrito Federal</span>
+                </footer>
+            </aside>
+
+            <div></div>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#FFF" />
+            </Link>
+        </div>
     );
 }
 
