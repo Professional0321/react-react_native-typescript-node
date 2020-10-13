978196b0a649f9a0509cbd0c0a60fa06f997e981
Professional0321 <web.dev.mar1212@gmail.com>
2020-10-13 16:05:16 -0300

    get all orphanages method

--- backend-api/src/controllers/OrphanagesController.ts
+++ backend-api/src/controllers/OrphanagesController.ts
@@ -4,6 +4,16 @@ import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanages';
 
 export default {
+
+    async index(request: Request, response: Response) {
+
+        const orphanagesRepository = getRepository(Orphanage);
+
+        const orphanages = await orphanagesRepository.find();
+
+        return response.json(orphanages);
+    },
+
     async create(request: Request, response: Response) {
         const {
             name,
