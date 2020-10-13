4478d51a0dd4a0b689485e5878ac2f2c962e2fe8
Professional0321 <web.dev.mar1212@gmail.com>
2020-10-13 18:32:09 -0300

    error handler

--- backend-api/src/server.ts
+++ backend-api/src/server.ts
@@ -2,12 +2,17 @@ import express from 'express';
 import path from 'path';
 
 import './database/connection';
+
+import 'express-async-errors';
+
 import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 app.listen(3333);
